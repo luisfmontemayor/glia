577b583f4fc77c0d1433b5cbb3ff793aa42cff0a
luisfmontemayor <lfelipemonte@gmail.com>
2025-12-11 17:22:09 +0100

    feat(helpers/getStagedScopes): made standalone staged file scopes maker

---
+++ scripts/getStagedScopes.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Written by Luis Felipe Montemayor, sometime around December of 2025
+from conventionalCommits import scopes
+
+if __name__ == "__main__":
+    for file in scopes.get_staged_scopes():
+        print(file)
