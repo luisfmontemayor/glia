146b6447773e6daaa46306e63b12302802af102b
luisfmontemayor <lfelipemonte@gmail.com>
2026-01-01 19:17:37 +0000

    refactor(None): changed camelCase names to snake_case

--- scripts/get_staged_scopes.py
+++ scripts/get_staged_scopes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Written by Luis Felipe Montemayor, sometime around December of 2025
-from conventionalCommits import scopes
+from conventional_commits import scopes
 
 if __name__ == "__main__":
     for file in scopes.get_staged_scopes():
