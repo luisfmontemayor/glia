f7f7a39dd91e1eebd3ec52883268dc0d8c576992
luisfmontemayor <lfelipemonte@gmail.com>
2026-01-01 21:00:52 +0000

    feat(libs/python/glia_common/logs): made glia_common logging

---
+++ libs/python/glia_common/src/glia_common/logs.py
@@ -0,0 +1,19 @@
+import logging
+import sys
+
+
+def setup_logger(name: str, level: int = logging.INFO) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stderr)
+
+        formatter = logging.Formatter(
+            fmt="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
