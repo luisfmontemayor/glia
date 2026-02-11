878dc7387681ec373818d2271fcafd5b0e6a38d7
luisfmontemayor <lfelipemonte@gmail.com>
2026-02-11 16:34:18 +0000

    fix(gliar/src/entrypoint): fixed _extendr suffix bug

--- gliar/src/entrypoint.c
+++ gliar/src/entrypoint.c
@@ -1,8 +1,8 @@
 #include <R.h>
 #include <Rinternals.h>
 
-extern void R_init_glia_core(DllInfo *dll);
+extern void R_init_gliar_extendr(DllInfo *dll);
 
 void R_init_gliar(DllInfo *dll) {
-    R_init_glia_core(dll);
+    R_init_gliar_extendr(dll);
 }
